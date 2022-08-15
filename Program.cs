85be9d69d030683fe0820f8a1f3c46d6d090fa35
Artur <buch16@bk.ru>
2022-08-15 17:38:12 +0300

    задания готовы к проверке

--- Program.cs
+++ Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int first = number / 1000;
+// int second = first % 10;
+// int foor = number / 10;
+// int five = foor % 10;
+// int first1 = number / 10000;
+// int num = number % 100;
+// int num1 = num % 10;
+
+
+// if (number < 10000 || number > 100000 )
+// {
+//    Console.WriteLine("Число не подходит под описание задания");
+// }
+// else if (number >= 10000 && number <= 100000 && second == five && first1 == num1)
+// {
+//    Console.WriteLine("Да");
+// }
+// else
+// {
+//    Console.WriteLine("Нет");
+// }
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+// между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.Write("Введите X1: ");
+// int x1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// int y1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите Z1: ");
+// int z1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// int x2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// int y2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите Z2: ");
+// int z2 = int.Parse(Console.ReadLine());
+
+// double d =Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+// Console.WriteLine($"d = {d:f2}");
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите число: ");
+// int number=Convert.ToInt32(Console.ReadLine());
+// int ind = 1;
+// if (number > 0)
+// {
+//     while (ind <= number)
+//     {
+//         Console.Write(Math.Pow(ind,3) + ", ");
+//         ind=ind+1;
+//     }
+// }
+// else
+// {
+//    while (ind >= number)
+//     {
+//         Console.Write(Math.Pow(ind,3) + ", ");
+//         ind=ind-1;
+//     }
+// }
+
